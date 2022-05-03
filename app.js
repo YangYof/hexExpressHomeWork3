4a0c3f3b23be517689ecf027b57637d002db6e6c
Cong yum <locryacoyu@hotmail.com>
2022-05-03 21:14:45 +0800

    postman is ok

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next)=>{
-    console.log(res.status);
     next()
 })
 app.use('/', indexRouter);
